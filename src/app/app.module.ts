e112fb741a63fba0a3dd97bdd69c5799dc601bc4
Abhilash Mishra <abhilash.mishra.10@gmail.com>
2021-11-01 20:39:39 +0530

    My First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { QuickSearchComponent } from './home/quick-search/quick-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SearchComponent,
+    QuickSearchComponent
   ],
   imports: [
     BrowserModule
