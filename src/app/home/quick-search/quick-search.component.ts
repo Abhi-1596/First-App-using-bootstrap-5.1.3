97637acfe24147a7cc562b7c26e194bd5238d91e
Abhilash Mishra <abhilash.mishra.10@gmail.com>
2021-11-18 17:29:54 +0530

    ...

--- src/app/home/quick-search/quick-search.component.ts
+++ src/app/home/quick-search/quick-search.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuickSearchComponent implements OnInit {
 
+  hotelTypes: any[] = [
+    {
+      "_id": 1,
+      "trip": "1",
+      "name": "Business",
+      "image": "https://i.ibb.co/8P41bJx/business.jpg",
+      "cost": 5000
+    },
+    {
+      "_id": 2,
+      "trip": "2",
+      "name": "Holiday",
+      "image": "https://i.ibb.co/CVNX8NZ/holiday.jpg",
+      "cost": 3000
+    },
+    {
+      "_id": 3,
+      "trip": "3",
+      "name": "Travel",
+      "image": "https://i.ibb.co/KrmBprr/travel.jpg",
+      "cost": 2000
+    },
+    {
+      "_id": 4,
+      "trip": "4",
+      "name": "Luxury",
+      "image": "https://i.ibb.co/HBc81qR/luxury.jpg",
+      "cost": 8000
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -11,43 +11,55 @@ export class SearchComponent implements OnInit {
     
 
   locations: any[] = [
-      {
-          "_id": 1,
-          "city_name": "Delhi",
-          "city": 1,
-          "country_name": "India"
-      },
-      {
-          "_id": 3,
-          "city_name": "Pune",
-          "city": 3,
-          "country_name": "India"
-      },
-      {
-          "_id": 2,
-          "city_name": "Mumbai",
-          "city": 2,
-          "country_name": "India"
-      },
-      {
-          "_id": 4,
-          "city_name": "Chandigarh",
-          "city": 4,
-          "country_name": "India"
-      },
-      {
-          "_id": 5,
-          "city_name": "Goa",
-          "city": 5,
-          "country_name": "India"
-      },
-      {
-          "_id": 6,
-          "city_name": "Manali",
-          "city": 6,
-          "country_name": "India"
-      }
-  ]
+    {
+        "_id": 1,
+        "city_name": "Delhi",
+        "city": 1,
+        "country_name": "India"
+    },
+    {
+        "_id": 3,
+        "city_name": "Pune",
+        "city": 3,
+        "country_name": "India"
+    },
+    {
+        "_id": 2,
+        "city_name": "Mumbai",
+        "city": 2,
+        "country_name": "India"
+    },
+    {
+        "_id": 4,
+        "city_name": "Chandigarh",
+        "city": 4,
+        "country_name": "India"
+    },
+    {
+        "_id": 5,
+        "city_name": "Goa",
+        "city": 5,
+        "country_name": "India"
+    },
+    {
+        "_id": 6,
+        "city_name": "Manali",
+        "city": 6,
+        "country_name": "India"
+    },
+    {
+        "_id": 7,
+        "city_name": "Indore",
+        "city": 7,
+        "country_name": "India"
+    },
+    {
+        "_id": 8,
+        "city_name": "Amsterdam",
+        "city": 8,
+        "country_name": "Netherlands"
+    }
+]
 
   constructor() { }
 
